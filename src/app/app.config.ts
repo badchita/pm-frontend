740560a55fcf890a76c5012c10162e7a32b237ab
badchita <47920377+badchita@users.noreply.github.com>
2025-11-17 11:23:03 +0800

    nz zoro style setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { en_US, NZ_I18N, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
-
+import { provideNzIcons } from 'ng-zorro-antd/icon';
+import * as AllIcons from '@ant-design/icons-angular/icons';
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideNzI18n(en_US), provideAnimationsAsync(), provideHttpClient()
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+
+    { provide: NZ_I18N, useValue: en_US },
+    provideNzIcons(Object.values(AllIcons)),
+  ],
 };
