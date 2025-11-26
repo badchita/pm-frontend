e06b634cdbe65c337b9290838bdb5c602272c216
badchita <47920377+badchita@users.noreply.github.com>
2025-11-26 10:44:47 +0800

    refractor folder and files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,14 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
-import { en_US, NZ_I18N, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { provideNzIcons } from 'ng-zorro-antd/icon';
 import * as AllIcons from '@ant-design/icons-angular/icons';
+import { routes } from './shared/app.routes';
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
