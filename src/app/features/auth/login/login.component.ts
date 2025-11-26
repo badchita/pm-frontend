a2e45645dbf242e0d2cd938dd511472ee179e43a
badchita <47920377+badchita@users.noreply.github.com>
2025-11-26 22:14:01 +0800

    login functionality

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,11 +1,13 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '@app/shared/services/api/auth.service';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCardComponent } from 'ng-zorro-antd/card';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -16,20 +18,39 @@ import { NzTypographyModule } from 'ng-zorro-antd/typography';
     NzButtonModule,
     NzCardComponent,
     NzTypographyModule,
-    RouterLink
-],
+    RouterLink,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent implements OnInit {
-  loginForm!: FormGroup;
-
+export class LoginComponent implements OnInit, OnDestroy {
+  private authService = inject(AuthService);
   private fb = inject(FormBuilder);
+
+  private _destroying$ = new Subject<void>();
+
+  loginForm!: FormGroup;
 
   ngOnInit() {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
+  }
+
+  login() {
+    const payload = this.loginForm.getRawValue();
+
+    this.authService
+      .login(payload)
+      .pipe(takeUntil(this._destroying$))
+      .subscribe((response) => {
+        console.log('Login response:', response);
+      });
+  }
+
+  ngOnDestroy() {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
   }
 }
