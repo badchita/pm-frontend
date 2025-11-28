1660b1c9866239f4be47b4b675d2e0871de62ab6
badchita <47920377+badchita@users.noreply.github.com>
2025-11-28 21:42:22 +0800

    layout setup

--- src/app/portal/portal.component.ts
+++ src/app/portal/portal.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@app/core/layout/header/header.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-portal',
-  imports: [RouterOutlet, HeaderComponent, NzLayoutModule],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    NzLayoutModule,
+    NzMenuModule,
+    NzIconModule,
+  ],
   templateUrl: './portal.component.html',
   styleUrl: './portal.component.scss',
 })
