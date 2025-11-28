773f6061f8f535a35143ac90d842b2c1af858d68
badchita <47920377+badchita@users.noreply.github.com>
2025-11-28 17:45:57 +0800

    initial sidebar setup

--- src/app/portal/portal.component.ts
+++ src/app/portal/portal.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@app/core/layout/header/header.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 @Component({
   selector: 'app-portal',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NzLayoutModule],
   templateUrl: './portal.component.html',
   styleUrl: './portal.component.scss',
 })
