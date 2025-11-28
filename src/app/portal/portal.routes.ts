a69ca5a60b7d3247bebe01df1c801873adedbc01
badchita <47920377+badchita@users.noreply.github.com>
2025-11-28 15:26:35 +0800

    porta route setup

--- src/app/portal/portal.routes.ts
+++ src/app/portal/portal.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { PortalComponent } from './portal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PortalComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('@app/features/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+      },
+    ],
+  },
+];
