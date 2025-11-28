f70abb2204466d6c1bcb7b9e68f212353f30ce70
badchita <47920377+badchita@users.noreply.github.com>
2025-11-28 22:05:50 +0800

    nav items

--- src/app/portal/portal.component.ts
+++ src/app/portal/portal.component.ts
@@ -1,20 +1,49 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@app/core/layout/header/header.component';
+import { NavItem } from '@app/shared/models/nav-item.model';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-portal',
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    NzLayoutModule,
-    NzMenuModule,
-    NzIconModule,
-  ],
+  imports: [RouterOutlet, HeaderComponent, NzLayoutModule, NzMenuModule, NzIconModule],
   templateUrl: './portal.component.html',
   styleUrl: './portal.component.scss',
 })
-export class PortalComponent {}
+export class PortalComponent {
+  private router = inject(Router);
+
+  navItem: NavItem[] = [
+    {
+      title: 'Dashboard',
+      icon: 'dashboard',
+      route: '/portal/dashboard',
+    },
+    {
+      title: 'Project List',
+      icon: 'unordered-list',
+      route: '/portal/project',
+    },
+    {
+      title: 'Task Board',
+      icon: 'paper-clip',
+      route: '/portal/task',
+    },
+    {
+      title: 'Profile Settings',
+      icon: 'setting',
+      route: '/portal/setting',
+    },
+    {
+      title: 'Application Settings',
+      icon: 'appstore',
+      route: '/portal/application',
+    },
+  ];
+
+  navigate(url: string) {
+    this.router.navigate([url]);
+  }
+}
