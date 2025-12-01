e667cd55f4ffaabf35e861acf9ff57c5f5950e76
badchita <47920377+badchita@users.noreply.github.com>
2025-12-01 15:44:27 +0800

    quick access component

--- src/app/features/dashboard/pages/home/home.component.ts
+++ src/app/features/dashboard/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { DashboardActiveProjectsComponent } from '../../components/dashboard-act
 import { DashboardUpcomingDeadlinesComponent } from '../../components/dashboard-upcoming-deadlines/dashboard-upcoming-deadlines.component';
 import { DashboardTaskCompletedComponent } from '../../components/dashboard-task-completed/dashboard-task-completed.component';
 import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { DashboardQuickAccessComponent } from '../../components/dashboard-quick-access/dashboard-quick-access.component';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,7 @@ import { NzButtonComponent } from 'ng-zorro-antd/button';
     DashboardUpcomingDeadlinesComponent,
     DashboardTaskCompletedComponent,
     NzButtonComponent,
+    DashboardQuickAccessComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
