38a6d9c5e94b0e52f6d27db66b8780fbac220a6d
badchita <47920377+badchita@users.noreply.github.com>
2025-12-01 18:25:10 +0800

    table action icons

--- src/app/features/project-list/components/project-list-table.component/project-list-table.component.ts
+++ src/app/features/project-list/components/project-list-table.component/project-list-table.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-project-list-table',
-  imports: [NzTableModule, NzDividerModule],
+  imports: [NzTableModule, NzDividerModule, NzButtonModule, NzIconModule],
   templateUrl: './project-list-table.component.html',
   styleUrl: './project-list-table.component.scss',
 })
