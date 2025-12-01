3a0cfe3cca90ad27020b16b3950899f92e1f00de
badchita <47920377+badchita@users.noreply.github.com>
2025-12-01 14:58:24 +0800

    ng2charts and chart js implementation

--- src/app/features/dashboard/components/active-projects.component/active-projects.component.ts
+++ src/app/features/dashboard/components/active-projects.component/active-projects.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-active-projects',
-  imports: [NzCardModule],
+  imports: [NzCardModule, BaseChartDirective],
   templateUrl: './active-projects.component.html',
   styleUrl: './active-projects.component.scss',
 })
-export class ActiveProjectsComponent {}
+export class ActiveProjectsComponent {
+  chartData = {
+    labels: ['Angular', 'Vue', 'Reactjs'],
+    datasets: [
+      {
+        axis: 'y',
+        data: [90, 85, 45],
+        fill: false,
+        backgroundColor: ['rgba(132, 11, 220, 1)'],
+      },
+    ],
+  };
+}
