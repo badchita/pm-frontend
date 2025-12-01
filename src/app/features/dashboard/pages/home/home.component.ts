556db13950460bda75ca18e03762eaa63906e011
badchita <47920377+badchita@users.noreply.github.com>
2025-12-01 15:32:52 +0800

    add new project button

--- src/app/features/dashboard/pages/home/home.component.ts
+++ src/app/features/dashboard/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DashboardActiveProjectsComponent } from '../../components/dashboard-active-projects/dashboard-active-projects.component';
 import { DashboardUpcomingDeadlinesComponent } from '../../components/dashboard-upcoming-deadlines/dashboard-upcoming-deadlines.component';
 import { DashboardTaskCompletedComponent } from '../../components/dashboard-task-completed/dashboard-task-completed.component';
+import { NzButtonComponent } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,7 @@ import { DashboardTaskCompletedComponent } from '../../components/dashboard-task
     DashboardActiveProjectsComponent,
     DashboardUpcomingDeadlinesComponent,
     DashboardTaskCompletedComponent,
+    NzButtonComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
