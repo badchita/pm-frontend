7d7320c0f6397c822401aa717bf0f030e4d59013
badchita <47920377+badchita@users.noreply.github.com>
2025-12-01 15:19:44 +0800

    upcoming deadlines component

--- src/app/features/dashboard/pages/home/home.component.ts
+++ src/app/features/dashboard/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DashboardActiveProjectsComponent } from '../../components/dashboard-active-projects/dashboard-active-projects.component';
+import { DashboardUpcomingDeadlinesComponent } from '../../components/dashboard-upcoming-deadlines/dashboard-upcoming-deadlines.component';
 
 @Component({
   selector: 'app-home',
-  imports: [DashboardActiveProjectsComponent],
+  imports: [DashboardActiveProjectsComponent, DashboardUpcomingDeadlinesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
