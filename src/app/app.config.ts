a22b242e7679841ea6f7e180c38bc1f3643acad0
badchita <47920377+badchita@users.noreply.github.com>
2025-12-02 21:05:28 +0800

    auth interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,12 @@ import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideNzIcons } from 'ng-zorro-antd/icon';
 import * as AllIcons from '@ant-design/icons-angular/icons';
 import { routes } from './shared/app.routes';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
@@ -22,5 +23,12 @@ export const appConfig: ApplicationConfig = {
     provideNzIcons(Object.values(AllIcons)),
 
     provideCharts(withDefaultRegisterables()),
+
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+    provideHttpClient(withInterceptorsFromDi())
   ],
 };
