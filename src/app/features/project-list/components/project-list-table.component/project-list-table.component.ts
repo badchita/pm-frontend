1c91da651272dcc864619312a1422f629233099c
badchita <47920377+badchita@users.noreply.github.com>
2025-12-02 10:16:52 +0800

    table input data and table ui adjustment

--- src/app/features/project-list/components/project-list-table.component/project-list-table.component.ts
+++ src/app/features/project-list/components/project-list-table.component/project-list-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzButtonModule } from 'ng-zorro-antd/button';
@@ -11,24 +11,5 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
   styleUrl: './project-list-table.component.scss',
 })
 export class ProjectListTableComponent {
-  listOfData = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-    },
-  ];
+  readonly tableData = input<any>();
 }
--- src/app/features/project-list/project-list.component.ts
+++ src/app/features/project-list/project-list.component.ts
@@ -7,4 +7,31 @@ import { ProjectListTableComponent } from './components/project-list-table.compo
   templateUrl: './project-list.component.html',
   styleUrl: './project-list.component.scss',
 })
-export class ProjectListComponent {}
+export class ProjectListComponent {
+  projectListData = [
+    {
+      id: 1,
+      projectName: 'Angular',
+      description:
+        'This interactive tutorial will teach you the basic building blocks to start building great apps with Angular.',
+      progress: 95,
+      dueDate: 'Oct 15, 2025',
+    },
+    {
+      id: 2,
+      projectName: 'ReactJs',
+      description:
+        'React lets you build user interfaces out of individual pieces called components. Create your own React components like Thumbnail, LikeButton, and Video. Then combine them into entire screens, pages, and apps.',
+      progress: 45,
+      dueDate: 'Oct 15, 2025',
+    },
+    {
+      id: 3,
+      projectName: 'Vuejs',
+      description:
+        'An approachable, performant and versatile framework for building web user interfaces.',
+      progress: 85,
+      dueDate: 'Oct 20, 2025',
+    },
+  ];
+}
