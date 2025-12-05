a83df143001fed89c8e07b3db6c9ca516a734875
badchita <47920377+badchita@users.noreply.github.com>
2025-12-05 20:53:57 +0800

    deactivate implementation

--- src/app/features/projects/edit-project/edit-project.component.ts
+++ src/app/features/projects/edit-project/edit-project.component.ts
@@ -177,20 +177,20 @@ export class EditProjectComponent implements OnInit, OnDestroy {
     );
 
     const id = this.editProjectForm.get('id')?.value;
+    const publishTitle = this.isPublished === 'N' ? 'Project Published' : 'Project Deactivated';
+    const publishMessage =
+      this.isPublished === 'N' ? 'ProjectFormPublishedSuccess' : 'ProjectFormDeactivatedSuccess';
 
     this.isLoading = this.projectService
-      .publish(id)
+      .publish(id, this.isPublished)
       .pipe(takeUntil(this._destroying$))
       .subscribe(
         (project) => {
           if (project) {
             this.notificationService.create(
               'success',
-              NOTIFICATION_TITLE.FormSuccess.replace('{{1}}', 'Project Published'),
-              NOTIFICATION_MESSAGE.ProjectFormPublishedSuccess.replace(
-                '{{1}}',
-                project.projectIdNumber
-              ),
+              NOTIFICATION_TITLE.FormSuccess.replace('{{1}}', publishTitle),
+              NOTIFICATION_MESSAGE[publishMessage].replace('{{1}}', project.projectIdNumber),
               {
                 nzClass: 'form-notification',
                 nzDuration: 5000,
