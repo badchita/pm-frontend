a121e4d42a5737873d26f4c309d2e127f2ad0098
badchita <47920377+badchita@users.noreply.github.com>
2025-12-06 14:40:31 +0800

    create edit task modal initial component

---
+++ src/app/shared/modal/create-edit-task-modal.component/create-edit-task-modal.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-edit-task-modal',
+  imports: [],
+  templateUrl: './create-edit-task-modal.component.html',
+  styleUrl: './create-edit-task-modal.component.scss',
+})
+export class CreateEditTaskModalComponent {}
