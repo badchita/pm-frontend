82b40bd746b56b2b0ba8c54edae984b59e1c3fc2
badchita <47920377+badchita@users.noreply.github.com>
2025-12-20 13:35:22 +0800

    alert component

---
+++ src/app/shared/components/error-alert/error-alert.component.ts
@@ -0,0 +1,52 @@
+import { Component, input, OnChanges, SimpleChanges } from '@angular/core';
+import { ALERT_DESCRIPTION, ALERT_MESAGE } from '@app/shared/constants/ui.constants';
+import { AlertType } from '@app/shared/models/alert.model';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+
+@Component({
+  selector: 'app-error-alert',
+  imports: [NzAlertModule],
+  templateUrl: './error-alert.component.html',
+  styleUrl: './error-alert.component.scss',
+})
+export class ErrorAlertComponent implements OnChanges {
+  error = input.required<any>();
+
+  alertDetails: AlertType | null = null;
+
+  ALERT_MESAGE = ALERT_MESAGE;
+  ALERT_DESCRIPTION = ALERT_DESCRIPTION;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['error'] && this.error) {
+      this.setAlert(this.error());
+    }
+  }
+
+  private setAlert(error: any) {
+    switch (error.status) {
+      case 0:
+        this.alertDetails = {
+          type: 'error',
+          message: this.ALERT_MESAGE.NoInternetConnection,
+          description: this.ALERT_DESCRIPTION.PleaseCheckYourNetworkAndTryAgain,
+        };
+        break;
+      case 409:
+        this.alertDetails = {
+          type: 'error',
+          message: this.ALERT_MESAGE.EmailAlreadyExists,
+          description: this.ALERT_DESCRIPTION.ThisEmailIsAlreadyInUse,
+        };
+        break;
+      case 500:
+      default:
+        this.alertDetails = {
+          type: 'error',
+          message: this.ALERT_MESAGE.UnexpectedErroIinternalServerError,
+          description: this.ALERT_DESCRIPTION.AnUnexpectedErrorOccurredPleaseTryAgainLater,
+        };
+        break;
+    }
+  }
+}
