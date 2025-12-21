154d7bd697cd373aad28df216f1d27fb728fb86c
badchita <47920377+badchita@users.noreply.github.com>
2025-12-21 18:36:19 +0800

    time line ui

--- src/app/features/tasks/components/task-form-history/task-form-history.component.ts
+++ src/app/features/tasks/components/task-form-history/task-form-history.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NzTimelineModule } from 'ng-zorro-antd/timeline';
 
 @Component({
   selector: 'app-task-form-history',
-  imports: [],
+  imports: [NzTimelineModule],
   templateUrl: './task-form-history.component.html',
   styleUrl: './task-form-history.component.scss',
 })
