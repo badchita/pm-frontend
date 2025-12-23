6e3abd098906d107c6ef246802470cb5eea4067e
badchita <47920377+badchita@users.noreply.github.com>
2025-12-23 11:42:07 +0800

    task comment reaction model

---
+++ src/app/features/tasks/models/task-comment-reaction-model.ts
@@ -0,0 +1,11 @@
+import { TaskCommentReactionType } from '@app/shared/enums/task-comment-reaction.enum';
+import { TaskComment } from './task-comment.model';
+
+export interface TaskCommentReaction {
+  id: number;
+  taskCommentId: number;
+  taskComment?: TaskComment;
+  userId: number;
+  reactionType?: TaskCommentReactionType | null;
+  createdAt: Date;
+}
