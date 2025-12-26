fd8f116ca3178caf167a562c9eca65f5bf894371
badchita <47920377+badchita@users.noreply.github.com>
2025-12-26 14:29:45 +0800

    taskboard service

---
+++ src/app/features/task-board/services/taskboard.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@app/environments/environment';
+import { Project } from '@app/features/projects/models/project.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskboardService {
+  private readonly http = inject(HttpClient);
+  private readonly apiProjects = `${environment.url}/projects`;
+
+  getProject(projectId: number): Observable<Project> {
+    return this.http.get<Project>(`${this.apiProjects}/${projectId}`);
+  }
+}
