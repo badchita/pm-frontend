0bc1c5b3e96e5d093be612b83b72b37712e2e379
badchita <47920377+badchita@users.noreply.github.com>
2025-12-26 09:45:33 +0800

    taskboard page component

---
+++ src/app/features/task-board/pages/task-board-page/task-board-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-board-page',
+  imports: [],
+  templateUrl: './task-board-page.component.html',
+  styleUrl: './task-board-page.component.scss',
+})
+export class TaskBoardPageComponent {}
