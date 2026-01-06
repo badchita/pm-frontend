16048a8abeabd7cfc884cb942528b6adfb46ae36
badchita <47920377+badchita@users.noreply.github.com>
2026-01-06 10:48:41 +0800

    admin tabs

--- src/app/features/admin/admin.component.ts
+++ src/app/features/admin/admin.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [NzTabsModule, NzLayoutModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
 })
-export class AdminComponent {}
+export class AdminComponent {
+  tabItems = [
+    {
+      title: 'Users',
+    },
+    {
+      title: 'Companies',
+    },
+    {
+      title: 'Projects',
+    },
+  ];
+}
