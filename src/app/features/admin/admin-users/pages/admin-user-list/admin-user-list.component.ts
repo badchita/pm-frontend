7026740911c8388d517f7dc161517a253e6bf7a7
badchita <47920377+badchita@users.noreply.github.com>
2026-01-06 11:08:12 +0800

    user list routing

---
+++ src/app/features/admin/admin-users/pages/admin-user-list/admin-user-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-user-list',
+  imports: [],
+  templateUrl: './admin-user-list.component.html',
+  styleUrl: './admin-user-list.component.scss',
+})
+export class AdminUserListComponent {}
--- src/app/features/admin/admin.component.ts
+++ src/app/features/admin/admin.component.ts
@@ -1,23 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
 
 @Component({
   selector: 'app-admin',
-  imports: [NzTabsModule, NzLayoutModule],
+  imports: [NzTabsModule, NzLayoutModule, RouterOutlet],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+  private readonly router = inject(Router);
+
   tabItems = [
     {
       title: 'Users',
+      route: '/portal/admin/users',
     },
     {
       title: 'Companies',
+      route: '/portal/admin/companies',
     },
     {
       title: 'Projects',
+      route: '/portal/admin/projects',
     },
   ];
+
+  activeIndex = 0;
+
+  ngOnInit() {
+    this.syncTabWithRoute();
+  }
+
+  onTabChange(index: number) {
+    this.router.navigateByUrl(this.tabItems[index].route);
+  }
+
+  syncTabWithRoute() {
+    const current = this.router.url;
+    const index = this.tabItems.findIndex((t) => current.startsWith(t.route));
+    this.activeIndex = index === -1 ? 0 : index;
+  }
 }
