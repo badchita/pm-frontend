e5fa11c4793d61a35681e3db4d122e18808e5a8e
badchita <47920377+badchita@users.noreply.github.com>
2026-01-07 11:04:37 +0800

    edit user setup

---
+++ src/app/features/admin/admin-users/pages/admin-edit-user/admin-edit-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-edit-user',
+  imports: [],
+  templateUrl: './admin-edit-user.component.html',
+  styleUrl: './admin-edit-user.component.scss',
+})
+export class AdminEditUserComponent {
+
+}
