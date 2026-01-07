2e8aa2b31fd48a8776ae2a181ba5abf9ea995e27
badchita <47920377+badchita@users.noreply.github.com>
2026-01-07 09:01:57 +0800

    user list search filter

--- src/app/features/admin/admin-users/pages/admin-user-list/components/admin-user-list-table/admin-user-list-table.component.ts
+++ src/app/features/admin/admin-users/pages/admin-user-list/components/admin-user-list-table/admin-user-list-table.component.ts
@@ -1,6 +1,6 @@
 import { DatePipe, I18nPluralPipe } from '@angular/common';
 import { Component, inject, input, OnDestroy, OnInit, output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { User } from '@app/features/auth/models/user.model';
 import { DataTable } from '@app/shared/models/data-table.model';
 import { NzButtonModule } from 'ng-zorro-antd/button';
@@ -10,6 +10,11 @@ import { NzTableModule, NzTableQueryParams } from 'ng-zorro-antd/table';
 import { debounceTime, Subject } from 'rxjs';
 import { UserRolePipe } from '../../pipes/user-role.pipe';
 import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { GenericUtilityService } from '@app/shared/services/generic-utility.service';
+import { UserStatusOptions } from '@app/shared/enums/search.enum';
 
 @Component({
   selector: 'app-admin-user-list-table',
@@ -22,6 +27,10 @@ import { NzTagModule } from 'ng-zorro-antd/tag';
     UserRolePipe,
     DatePipe,
     NzTagModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzInputModule,
+    NzSelectModule,
   ],
   templateUrl: './admin-user-list-table.component.html',
   styleUrl: './admin-user-list-table.component.scss',
@@ -33,10 +42,13 @@ export class AdminUserListTableComponent implements OnInit, OnDestroy {
   readonly onUpdateTable = output<NzTableQueryParams>();
 
   private readonly formBuilder = inject(FormBuilder);
+  private readonly genericUtilityService = inject(GenericUtilityService);
 
   private readonly _destroying$ = new Subject<void>();
 
   searchUserForm!: FormGroup;
+
+  userStatusOptions = this.genericUtilityService.objectToArray(UserStatusOptions);
 
   ngOnInit() {
     this.buildForm();
@@ -45,7 +57,7 @@ export class AdminUserListTableComponent implements OnInit, OnDestroy {
   buildForm() {
     this.searchUserForm = this.formBuilder.group({
       search: [null],
-      dueDate: [null],
+      isApproved: [null],
       isPublished: [null],
     });
 
@@ -55,7 +67,7 @@ export class AdminUserListTableComponent implements OnInit, OnDestroy {
       .subscribe(() => this.updateTable());
 
     this.searchUserForm
-      .get('dueDate')
+      .get('isApproved')
       ?.valueChanges.pipe(debounceTime(500))
       .subscribe(() => this.updateTable());
 
@@ -79,6 +91,10 @@ export class AdminUserListTableComponent implements OnInit, OnDestroy {
     };
 
     this.onUpdateTable.emit(tableParams);
+  }
+
+  reset() {
+    this.searchUserForm.reset();
   }
 
   ngOnDestroy() {
--- src/app/shared/enums/search.enum.ts
+++ src/app/shared/enums/search.enum.ts
@@ -1,9 +1,14 @@
 export enum Status {
   Active = 'Y',
-  Inactive = 'N'
+  Inactive = 'N',
 }
 
 export const StatusOptions: Record<Status, string> = {
   [Status.Active]: 'Active',
-  [Status.Inactive]: 'Inactive'
-}
+  [Status.Inactive]: 'Inactive',
+};
+
+export const UserStatusOptions: Record<Status, string> = {
+  [Status.Active]: 'Approved',
+  [Status.Inactive]: 'Pending',
+};
