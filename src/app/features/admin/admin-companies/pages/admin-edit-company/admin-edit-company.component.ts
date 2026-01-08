29fdc4b37efbb8b5806a82799ab1e6ce4952c567
badchita <47920377+badchita@users.noreply.github.com>
2026-01-08 14:27:29 +0800

    feat: edit company component setup

---
+++ src/app/features/admin/admin-companies/pages/admin-edit-company/admin-edit-company.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-edit-company',
+  imports: [],
+  templateUrl: './admin-edit-company.component.html',
+  styleUrl: './admin-edit-company.component.scss',
+})
+export class AdminEditCompanyComponent {
+
+}
