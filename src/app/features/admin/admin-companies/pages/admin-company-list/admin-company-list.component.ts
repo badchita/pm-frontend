f9bb8aa25bc00620a06c0cac31bc263e157d838e
badchita <47920377+badchita@users.noreply.github.com>
2026-01-08 11:29:35 +0800

    feat: admin company list component setup

---
+++ src/app/features/admin/admin-companies/pages/admin-company-list/admin-company-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-company-list',
+  imports: [],
+  templateUrl: './admin-company-list.component.html',
+  styleUrl: './admin-company-list.component.scss',
+})
+export class AdminCompanyListComponent {
+
+}
