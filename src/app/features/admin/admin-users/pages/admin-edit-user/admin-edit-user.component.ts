eead2d6fba13f4c2831d8e9adec46519c5953565
badchita <47920377+badchita@users.noreply.github.com>
2026-01-08 22:56:23 +0800

    feat: load companies

--- src/app/features/admin/admin-users/pages/admin-edit-user/admin-edit-user.component.ts
+++ src/app/features/admin/admin-users/pages/admin-edit-user/admin-edit-user.component.ts
@@ -2,6 +2,8 @@ import { DatePipe } from '@angular/common';
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Company } from '@app/features/admin/admin-companies/models/company.model';
+import { CompanyService } from '@app/features/admin/admin-companies/services/company.service';
 import { ErrorAlertComponent } from '@app/shared/components/error-alert/error-alert.component';
 import {
   NOTIFICATION_MESSAGE,
@@ -22,7 +24,7 @@ import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { NzTagModule } from 'ng-zorro-antd/tag';
-import { finalize, Subject, takeUntil } from 'rxjs';
+import { finalize, forkJoin, Observable, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-admin-edit-user',
@@ -44,6 +46,7 @@ import { finalize, Subject, takeUntil } from 'rxjs';
 })
 export class AdminEditUserComponent implements OnInit, OnDestroy {
   private readonly userService = inject(UserService);
+  private readonly companyService = inject(CompanyService);
   private readonly genericUtilityService = inject(GenericUtilityService);
   private readonly notificationService = inject(NzNotificationService);
   private readonly formBuilder = inject(FormBuilder);
@@ -58,6 +61,7 @@ export class AdminEditUserComponent implements OnInit, OnDestroy {
   userStatus!: string;
   userCreated!: Date | string;
   originalUser: any;
+  companies!: { label: string; value: number }[];
 
   isLoading = false;
   roleOptions = this.genericUtilityService.objectToArray(UserRoleOptions, false, true);
@@ -70,7 +74,7 @@ export class AdminEditUserComponent implements OnInit, OnDestroy {
 
     this.route.paramMap.subscribe((params) => {
       const id = params.get('id')!;
-      this.loadUser(id);
+      this.loadData(id);
     });
   }
 
@@ -85,11 +89,13 @@ export class AdminEditUserComponent implements OnInit, OnDestroy {
     });
   }
 
-  loadUser(id: string) {
+  loadData(id: string) {
     this.isLoading = true;
 
-    this.userService
-      .getById(+id)
+    forkJoin({
+      user: this.userService.getById(+id),
+      companies: this.loadCompanies({}),
+    })
       .pipe(
         takeUntil(this._destroying$),
         finalize(() => {
@@ -97,7 +103,7 @@ export class AdminEditUserComponent implements OnInit, OnDestroy {
         })
       )
       .subscribe({
-        next: (user) => {
+        next: ({ user, companies }) => {
           this.userStatus = user.isApproved;
           this.userCreated = user.createdAt;
           this.originalUser = { ...user };
@@ -110,11 +116,20 @@ export class AdminEditUserComponent implements OnInit, OnDestroy {
               (key) => value[key] !== this.originalUser[key]
             );
           });
+
+          this.companies = companies.map((company) => ({
+            label: company.name,
+            value: company.id,
+          }));
         },
         error: (error) => {
           this.catchError = error;
         },
       });
+  }
+
+  loadCompanies(filter?: any): Observable<Company[]> {
+    return this.companyService.getSearchCompanies(filter);
   }
 
   close() {
