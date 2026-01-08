c359d1edf635cde4491f825718b42111a6ad2383
badchita <47920377+badchita@users.noreply.github.com>
2026-01-08 11:34:08 +0800

    feat: company list table component setup

---
+++ src/app/features/admin/admin-companies/pages/admin-company-list/components/admin-company-list-table/admin-company-list-table.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-company-list-table',
+  imports: [],
+  templateUrl: './admin-company-list-table.component.html',
+  styleUrl: './admin-company-list-table.component.scss',
+})
+export class AdminCompanyListTableComponent {}
