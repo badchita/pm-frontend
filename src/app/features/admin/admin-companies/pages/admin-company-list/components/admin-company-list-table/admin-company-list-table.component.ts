3effba5d6ddd41ce3be20678ef73c33d7e8875e4
badchita <47920377+badchita@users.noreply.github.com>
2026-01-08 11:36:14 +0800

    feat: company list table UI

--- src/app/features/admin/admin-companies/pages/admin-company-list/components/admin-company-list-table/admin-company-list-table.component.ts
+++ src/app/features/admin/admin-companies/pages/admin-company-list/components/admin-company-list-table/admin-company-list-table.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTagModule } from 'ng-zorro-antd/tag';
 
 @Component({
   selector: 'app-admin-company-list-table',
-  imports: [],
+  imports: [NzTableModule, NzTagModule],
   templateUrl: './admin-company-list-table.component.html',
   styleUrl: './admin-company-list-table.component.scss',
 })
