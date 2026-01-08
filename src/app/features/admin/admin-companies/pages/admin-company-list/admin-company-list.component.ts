4ad219f0e6a01542ddc5e076fd10adb6d2901aa3
badchita <47920377+badchita@users.noreply.github.com>
2026-01-08 11:31:09 +0800

    feat: company list UI

--- src/app/features/admin/admin-companies/pages/admin-company-list/admin-company-list.component.ts
+++ src/app/features/admin/admin-companies/pages/admin-company-list/admin-company-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ErrorAlertComponent } from '@app/shared/components/error-alert/error-alert.component';
 
 @Component({
   selector: 'app-admin-company-list',
-  imports: [],
+  imports: [ErrorAlertComponent],
   templateUrl: './admin-company-list.component.html',
   styleUrl: './admin-company-list.component.scss',
 })
 export class AdminCompanyListComponent {
-
+  catchError!: any;
 }
