16c8f12b2594a842d369c9d0882f0fa7bcb157e0
badchita <47920377+badchita@users.noreply.github.com>
2026-01-15 14:27:16 +0800

    feat: login and back buttons

--- src/app/features/errors/pages/not-found/not-found.component.ts
+++ src/app/features/errors/pages/not-found/not-found.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@app/features/auth/services/auth.service';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [NzButtonModule, NzIconModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+  location = inject(Location);
 
+  isLoggedIn = false;
+
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
+
+  login() {
+    this.router.navigate(['/login']);
+  }
 }
