0dee89e8706c30242dbb55590d0e93b62be9886a
badchita <47920377+badchita@users.noreply.github.com>
2026-01-16 11:07:05 +0800

    feat: forbidden UI

--- src/app/features/errors/pages/forbidden/forbidden.component.ts
+++ src/app/features/errors/pages/forbidden/forbidden.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-forbidden',
-  imports: [],
+  imports: [NzButtonModule, NzIconModule],
   templateUrl: './forbidden.component.html',
   styleUrl: './forbidden.component.scss',
 })
-export class ForbiddenComponent {
-
-}
+export class ForbiddenComponent {}
