6c9661ec57d23764f7d6c8d2f1590aedab48f64e
badchita <47920377+badchita@users.noreply.github.com>
2026-01-20 11:43:08 +0800

    feat: header logout button

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [NzIconModule],
+  imports: [NzIconModule, NzButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
