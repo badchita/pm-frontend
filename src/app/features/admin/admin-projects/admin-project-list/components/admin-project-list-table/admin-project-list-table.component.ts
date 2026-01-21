e2f34fbe6afb285bfe90dd4cb144261b06cb8879
badchita <47920377+badchita@users.noreply.github.com>
2026-01-21 14:14:49 +0800

    feat: fetching projects

--- src/app/features/admin/admin-projects/admin-project-list/components/admin-project-list-table/admin-project-list-table.component.ts
+++ src/app/features/admin/admin-projects/admin-project-list/components/admin-project-list-table/admin-project-list-table.component.ts
@@ -1,7 +1,9 @@
 import { DatePipe, I18nPluralPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { UserRolePipe } from '@app/features/admin/admin-users/pages/admin-user-list/pipes/user-role.pipe';
+import { Project } from '@app/features/projects/models/project.model';
+import { DataTable } from '@app/shared/models/data-table.model';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzFormModule } from 'ng-zorro-antd/form';
@@ -9,8 +11,9 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzSelectModule } from 'ng-zorro-antd/select';
-import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTableModule, NzTableQueryParams } from 'ng-zorro-antd/table';
 import { NzTagModule } from 'ng-zorro-antd/tag';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-admin-project-list-table',
@@ -32,4 +35,66 @@ import { NzTagModule } from 'ng-zorro-antd/tag';
   templateUrl: './admin-project-list-table.component.html',
   styleUrl: './admin-project-list-table.component.scss',
 })
-export class AdminProjectListTableComponent {}
+export class AdminProjectListTableComponent implements OnInit {
+  readonly dataTable = input.required<DataTable<Project>>();
+  readonly dataList = input.required<Project[] | []>();
+  readonly loading = input.required<boolean>();
+  readonly onUpdateTable = output<NzTableQueryParams>();
+
+  private readonly formBuilder = inject(FormBuilder);
+
+  searchUserForm!: FormGroup;
+
+  isDeleted = 'N';
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.searchUserForm = this.formBuilder.group({
+      search: [null],
+      isApproved: [null],
+      companyId: [null],
+      role: [null],
+    });
+
+    this.searchUserForm
+      .get('search')
+      ?.valueChanges.pipe(debounceTime(500))
+      .subscribe(() => this.updateTable());
+
+    this.searchUserForm
+      .get('isApproved')
+      ?.valueChanges.pipe(debounceTime(500))
+      .subscribe(() => this.updateTable());
+
+    this.searchUserForm
+      .get('companyId')
+      ?.valueChanges.pipe(debounceTime(500))
+      .subscribe(() => this.updateTable());
+
+    this.searchUserForm
+      .get('role')
+      ?.valueChanges.pipe(debounceTime(500))
+      .subscribe(() => this.updateTable());
+  }
+
+  updateTable(params?: NzTableQueryParams) {
+    const searchFormValues = this.searchUserForm.getRawValue();
+    const isFiltering =
+      searchFormValues.search || searchFormValues.description || searchFormValues.dueDate;
+    const filter: NzTableQueryParams['filter'] = [
+      { ...searchFormValues, isDeleted: this.isDeleted },
+    ];
+
+    const tableParams: NzTableQueryParams = {
+      pageIndex: isFiltering ? 1 : (params?.pageIndex ?? this.dataTable().page),
+      pageSize: params?.pageSize ?? this.dataTable().pageSize,
+      sort: params?.sort ?? [],
+      filter: filter,
+    };
+
+    this.onUpdateTable.emit(tableParams);
+  }
+}
