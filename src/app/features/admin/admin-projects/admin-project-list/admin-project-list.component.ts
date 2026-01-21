31b1474091a034fd245ac698da8edf3a7dacd44c
badchita <47920377+badchita@users.noreply.github.com>
2026-01-21 11:06:13 +0800

    feat: admin project list component setup

---
+++ src/app/features/admin/admin-projects/admin-project-list/admin-project-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-project-list',
+  imports: [],
+  templateUrl: './admin-project-list.component.html',
+  styleUrl: './admin-project-list.component.scss',
+})
+export class AdminProjectListComponent {
+
+}
