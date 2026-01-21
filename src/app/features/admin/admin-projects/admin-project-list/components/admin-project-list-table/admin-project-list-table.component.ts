41d70fa5744aa3791cd488b48bc9cfa5f3c67047
badchita <47920377+badchita@users.noreply.github.com>
2026-01-21 11:17:11 +0800

    feat: admin project list table UI

--- src/app/features/admin/admin-projects/admin-project-list/components/admin-project-list-table/admin-project-list-table.component.ts
+++ src/app/features/admin/admin-projects/admin-project-list/components/admin-project-list-table/admin-project-list-table.component.ts
@@ -1,11 +1,35 @@
+import { DatePipe, I18nPluralPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UserRolePipe } from '@app/features/admin/admin-users/pages/admin-user-list/pipes/user-role.pipe';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTagModule } from 'ng-zorro-antd/tag';
 
 @Component({
   selector: 'app-admin-project-list-table',
-  imports: [],
+  imports: [
+    NzTableModule,
+    NzIconModule,
+    NzDividerModule,
+    I18nPluralPipe,
+    NzButtonModule,
+    UserRolePipe,
+    DatePipe,
+    NzTagModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzInputModule,
+    NzSelectModule,
+    NzModalModule,
+  ],
   templateUrl: './admin-project-list-table.component.html',
   styleUrl: './admin-project-list-table.component.scss',
 })
-export class AdminProjectListTableComponent {
-
-}
+export class AdminProjectListTableComponent {}
